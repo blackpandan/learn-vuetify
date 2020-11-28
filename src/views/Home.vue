39344755f5317af4a83e403128c3a37e4c7f4386
blackpandan <faithgstc54@gmail.com>
2020-11-28 14:33:38 -0500

    intial stage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="display-1 font-weight-bold blue white--text">Home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae fuga dignissimos accusamus molestiae explicabo itaque cumque alias incidunt tempore voluptatibus, vel debitis. Aliquam veniam praesentium eos, autem repudiandae provident consequuntur!</p>
+    <p class="caption red--text">this is a caption</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
